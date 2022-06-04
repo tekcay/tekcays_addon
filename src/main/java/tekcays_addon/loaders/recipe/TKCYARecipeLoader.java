63076ec9ea7aa5a5fc3adba7d7795e5bf39ae844
tekcay <pierreblc@hotmail.fr>
2022-06-04 19:30:38 +0200

    removed MaterialRecipeHandler

--- src/main/java/tekcays_addon/loaders/recipe/TKCYARecipeLoader.java
+++ src/main/java/tekcays_addon/loaders/recipe/TKCYARecipeLoader.java
@@ -1,10 +1,8 @@
 package tekcays_addon.loaders.recipe;
 
-import tekcays_addon.loaders.recipe.handlers.TKCYAMaterialRecipeHandler;
-
 public class TKCYARecipeLoader {
 
     public static void init() {
-        TKCYAMaterialRecipeHandler.register();
+
     }
 }
