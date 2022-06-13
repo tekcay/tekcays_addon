ae853e2925a07107e27ae3a35243adf938ce2dc0
tekcay <pierreblc@hotmail.fr>
2022-06-14 00:01:01 +0200

    fixed recipes

--- src/main/java/tekcays_addon/loaders/recipe/ShapedCraftingRecipes.java
+++ src/main/java/tekcays_addon/loaders/recipe/ShapedCraftingRecipes.java
@@ -4,8 +4,6 @@ import gregtech.api.recipes.ModHandler;
 import gregtech.api.unification.OreDictUnifier;
 import gregtech.api.unification.material.Material;
 import gregtech.api.unification.ore.OrePrefix;
-import gregtech.api.unification.stack.UnificationEntry;
-
 
 import static tekcays_addon.api.unification.TKCYAMaterialFlagAddition.MOLD_MATERIALS;
 import static tekcays_addon.api.unification.material.ore.TKCYAOrePrefix.*;
@@ -16,16 +14,24 @@ public class ShapedCraftingRecipes{
 
         for (Material m : MOLD_MATERIALS) {
 
-            ModHandler.addShapedRecipe("mold_empty", OreDictUnifier.get(moldEmpty, m), "hf", "PP", "PP", 'P', new UnificationEntry(OrePrefix.plate, m));
-            
-            ModHandler.addShapedRecipe("mold_rotor", OreDictUnifier.get(moldEmpty, m), "  h", " S ", "   ", 'S', OreDictUnifier.get(moldEmpty, m));
-            ModHandler.addShapedRecipe("mold_gear_small", OreDictUnifier.get(moldGearSmall, m), "   ", "   ", "h S", 'S', OreDictUnifier.get(moldEmpty, m));
-            ModHandler.addShapedRecipe("mold_cylinder", OreDictUnifier.get(moldCylinder, m), "  S", "   ", "  h", 'S', OreDictUnifier.get(moldEmpty, m));
-            ModHandler.addShapedRecipe("mold_ball", OreDictUnifier.get(moldBall, m), "   ", " S ", "h  ", 'S', OreDictUnifier.get(moldEmpty, m));
-            ModHandler.addShapedRecipe("mold_ingot", OreDictUnifier.get(moldIngot, m), "   ", " S ", " h ", 'S', OreDictUnifier.get(moldEmpty, m));
-            ModHandler.addShapedRecipe("mold_bolt", OreDictUnifier.get(moldBolt, m), "   ", " S ", "  h", 'S', OreDictUnifier.get(moldEmpty, m));
-            ModHandler.addShapedRecipe("mold_gear", OreDictUnifier.get(moldGear, m), "   ", " Sh", "   ", 'S', OreDictUnifier.get(moldEmpty, m));
-            ModHandler.addShapedRecipe("mold_plate", OreDictUnifier.get(moldPlate, m), " h ", " S ", "   ", 'S', OreDictUnifier.get(moldEmpty, m));
+            ModHandler.addShapedRecipe("mold_empty" + m.getUnlocalizedName(), OreDictUnifier.get(moldEmpty, m),
+                    "hf", "PP", "PP", 'P', OreDictUnifier.get(OrePrefix.plate, m));
+            ModHandler.addShapedRecipe("mold_rotor" + m.getUnlocalizedName(), OreDictUnifier.get(moldEmpty, m),
+                    "  h", " S ", "   ", 'S', OreDictUnifier.get(moldEmpty, m));
+            ModHandler.addShapedRecipe("mold_gear_small" + m.getUnlocalizedName(), OreDictUnifier.get(moldGearSmall, m),
+                    "   ", "   ", "h S", 'S', OreDictUnifier.get(moldEmpty, m));
+            ModHandler.addShapedRecipe("mold_cylinder" + m.getUnlocalizedName(), OreDictUnifier.get(moldCylinder, m),
+                    "  S", "   ", "  h", 'S', OreDictUnifier.get(moldEmpty, m));
+            ModHandler.addShapedRecipe("mold_ball" + m.getUnlocalizedName(), OreDictUnifier.get(moldBall, m),
+                    "   ", " S ", "h  ", 'S', OreDictUnifier.get(moldEmpty, m));
+            ModHandler.addShapedRecipe("mold_ingot" + m.getUnlocalizedName(), OreDictUnifier.get(moldIngot, m),
+                    "   ", " S ", " h ", 'S', OreDictUnifier.get(moldEmpty, m));
+            ModHandler.addShapedRecipe("mold_bolt" + m.getUnlocalizedName(), OreDictUnifier.get(moldBolt, m),
+                    "   ", " S ", "  h", 'S', OreDictUnifier.get(moldEmpty, m));
+            ModHandler.addShapedRecipe("mold_gear" + m.getUnlocalizedName(), OreDictUnifier.get(moldGear, m),
+                    "   ", " Sh", "   ", 'S', OreDictUnifier.get(moldEmpty, m));
+            ModHandler.addShapedRecipe("mold_plate" + m.getUnlocalizedName(), OreDictUnifier.get(moldPlate, m),
+                    " h ", " S ", "   ", 'S', OreDictUnifier.get(moldEmpty, m));
         
         }
 
