64dd2c37a287d31e904afeb0a0ac1f721ef1c982
tekcay <pierreblc@hotmail.fr>
2022-06-25 22:20:57 +0200

    fixed recipe duration

--- src/main/java/tekcays_addon/loaders/recipe/handlers/GalvanizingRecipeHandler.java
+++ src/main/java/tekcays_addon/loaders/recipe/handlers/GalvanizingRecipeHandler.java
@@ -4,10 +4,10 @@ import gregtech.api.GTValues;
 import gregtech.api.recipes.RecipeMaps;
 import gregtech.api.unification.ore.OrePrefix;
 import tekcays_addon.api.recipes.TKCYARecipeMaps;
-import tekcays_addon.api.unification.TKCYAMaterials;
 
 import static gregtech.api.GTValues.LV;
 import static gregtech.api.unification.material.Materials.*;
+import static tekcays_addon.api.unification.TKCYAMaterials.GalvanizedSteel;
 
 public class GalvanizingRecipeHandler {
 
@@ -15,21 +15,22 @@ public class GalvanizingRecipeHandler {
 
         for (OrePrefix orePrefix : OrePrefix.values()) {
 
-            if(!orePrefix.doGenerateItem(TKCYAMaterials.GalvanizedSteel)) continue;
+            if(!orePrefix.doGenerateItem(Steel)) continue;
+            if(!orePrefix.doGenerateItem(GalvanizedSteel)) continue;
 
             RecipeMaps.CHEMICAL_BATH_RECIPES.recipeBuilder()
                     .input(orePrefix, Steel)
                     .fluidInputs(Zinc.getFluid((int) (orePrefix.getMaterialAmount(Steel) * GTValues.L / (GTValues.M * 9))))
-                    .duration((int) (orePrefix.getMaterialAmount(Steel) / GTValues.M))
-                    .output(orePrefix, TKCYAMaterials.GalvanizedSteel)
+                    .duration((int) (Steel.getMass() * orePrefix.getMaterialAmount(Steel) / GTValues.M))
+                    .output(orePrefix, GalvanizedSteel)
                     .EUt((int) GTValues.V[LV])
                     .buildAndRegister();
 
             TKCYARecipeMaps.PRIMITIVE_BATH.recipeBuilder()
                     .input(orePrefix, Steel)
                     .fluidInputs(Zinc.getFluid((int) (orePrefix.getMaterialAmount(Steel) * GTValues.L / (GTValues.M * 9))))
-                    .output(orePrefix, TKCYAMaterials.GalvanizedSteel)
-                    .duration((int) (4 * orePrefix.getMaterialAmount(Steel) / GTValues.M))
+                    .output(orePrefix, GalvanizedSteel)
+                    .duration((int) (4 * Steel.getMass() * orePrefix.getMaterialAmount(Steel) / GTValues.M))
                     .buildAndRegister();
 
         }
