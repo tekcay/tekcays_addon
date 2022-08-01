677537cfda693f9003dbbbaabbf19f51ffe19eab
tekcay <pierreblc@hotmail.fr>
2022-08-01 18:24:38 +0200

    added a blast furnace per type of brick

--- src/main/java/tekcays_addon/common/metatileentities/multi/MetaTileEntityTKCYABlastFurnace.java
+++ src/main/java/tekcays_addon/common/metatileentities/multi/MetaTileEntityTKCYABlastFurnace.java
@@ -12,10 +12,10 @@ import gregtech.api.pattern.PatternMatchContext;
 import gregtech.api.pattern.TraceabilityPredicate;
 import gregtech.api.recipes.Recipe;
 import gregtech.client.renderer.ICubeRenderer;
-import gregtech.client.renderer.texture.Textures;
 import gregtech.client.renderer.texture.cube.SimpleOverlayRenderer;
 import gregtech.common.ConfigHolder;
 import gregtech.common.items.MetaItems;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
@@ -33,11 +33,11 @@ import net.minecraftforge.fluids.IFluidTank;
 import net.minecraftforge.items.ItemStackHandler;
 import tekcays_addon.api.capability.impl.MultiParallelLogic;
 import tekcays_addon.api.metatileentity.mutiblock.RecipeMapMultiblockNoEnergyController;
-import tekcays_addon.api.pattern.TKCYATraceabilityPredicate;
 import tekcays_addon.api.recipes.TKCYARecipeMaps;
 import tekcays_addon.api.recipes.builders.TemperatureRecipeBuilder;
 import tekcays_addon.api.render.TKCYATextures;
 import tekcays_addon.api.unification.TKCYAMaterials;
+import tekcays_addon.common.blocks.TKCYAMetaBlocks;
 import tekcays_addon.common.blocks.blocks.BlockBrick;
 import tekcays_addon.common.items.TKCYAMetaItems;
 
@@ -53,7 +53,8 @@ import static tekcays_addon.api.utils.TKCYAValues.*;
 public class MetaTileEntityTKCYABlastFurnace extends RecipeMapMultiblockNoEnergyController {
 
 
-    private SimpleOverlayRenderer texture;
+    private final BlockBrick.BrickType brick;
+    private final IBlockState iBlockState;
     private boolean canAchieveTargetTemp;
     private boolean hasEnoughGas;
     private boolean hasEnoughFluidHeatingValue, hasEnoughItemHeatingValue;
@@ -80,18 +81,13 @@ public class MetaTileEntityTKCYABlastFurnace extends RecipeMapMultiblockNoEnergy
         increaseTemp = 1 + inputItemMultiplier * inputFluidMultiplier;
     }
 
-    /*
-    public void setTargetTemp() {
-        targetTemp = 2000;
-    }
 
-     */
-
-    public MetaTileEntityTKCYABlastFurnace(ResourceLocation metaTileEntityId) {
+    public MetaTileEntityTKCYABlastFurnace(ResourceLocation metaTileEntityId, BlockBrick.BrickType brick) {
         super(metaTileEntityId, TKCYARecipeMaps.BLASTING_RECIPES);
+        this.brick = brick;
+        this.targetTemp = brick.getBrickTemperature();
+        this.iBlockState = TKCYAMetaBlocks.BLOCK_BRICK.getState(brick);
         this.recipeMapWorkable = new MultiParallelLogic(this);
-
-        texture = BlockBrick.BrickType.BRICK.getTexture();
 
         temp = 300;
 
@@ -437,7 +433,13 @@ public class MetaTileEntityTKCYABlastFurnace extends RecipeMapMultiblockNoEnergy
 
     @Override
     public ICubeRenderer getBaseTexture(IMultiblockPart sourcePart) {
-        return Textures.PRIMITIVE_BRICKS;
+
+        if (brick.equals(BlockBrick.BrickType.BRICK)) return TKCYATextures.BRICK;
+        if (brick.equals(BlockBrick.BrickType.FIRECLAY_BRICK)) return TKCYATextures.FIRECLAY_BRICK;
+        if (brick.equals(BlockBrick.BrickType.REINFORCED_BRICK)) return TKCYATextures.REINFORCED_BRICK;
+        if (brick.equals(BlockBrick.BrickType.STRONG_BRICK)) return TKCYATextures.STRONG_BRICK;
+
+        return TKCYATextures.BRICK;
     }
 
     @Override
@@ -445,6 +447,16 @@ public class MetaTileEntityTKCYABlastFurnace extends RecipeMapMultiblockNoEnergy
         return TKCYATextures.ELECTRIC_MELTER_OVERLAY;
     }
 
+    protected IBlockState getCasingState() {
+        return iBlockState;
+
+        /*
+        if (brick.equals(BlockBrick.BrickType.BRICK)) return TKCYAMetaBlocks.BLOCK_BRICK.getState(BlockBrick.BrickType.BRICK);
+        //return TKCYAMetaBlocks.BLOCK_BRICK.getState(brick);
+        return MetaBlocks.METAL_CASING.getState(BlockMetalCasing.MetalCasingType.PRIMITIVE_BRICKS);
+
+         */
+    }
 
     @Override
     protected BlockPattern createStructurePattern() {
@@ -456,11 +468,11 @@ public class MetaTileEntityTKCYABlastFurnace extends RecipeMapMultiblockNoEnergy
                 //Particle
                 //.aisle("#####", "#####", "##C##", "#####", "#####").setRepeatable(0, 1)
                 .where('S', selfPredicate())
-                .where('Y', TKCYATraceabilityPredicate.BLOCK_BRICKS.get())
-                .where('X', TKCYATraceabilityPredicate.BLOCK_BRICKS.get()
+                .where('Y', states(getCasingState()))
+                .where('X', states(getCasingState())
                         .or(abilities(MultiblockAbility.IMPORT_FLUIDS).setExactLimit(1))
                         .or(abilities(MultiblockAbility.EXPORT_FLUIDS).setExactLimit(1)))
-                .where('O', TKCYATraceabilityPredicate.BLOCK_BRICKS.get()
+                .where('O', states(getCasingState())
                         .or(abilities(MultiblockAbility.EXPORT_FLUIDS).setMaxGlobalLimited(2))
                         .or(abilities(MultiblockAbility.IMPORT_ITEMS).setMinGlobalLimited(1,1).setMaxGlobalLimited(2)))
                 .where('I', heightIndicatorPredicate())
@@ -486,7 +498,6 @@ public class MetaTileEntityTKCYABlastFurnace extends RecipeMapMultiblockNoEnergy
     public void invalidateStructure() {
         setTemp(300);
         setIncreaseTemp();
-        //setTargetTemp();
         super.invalidateStructure();
         resetTileAbilities();
     }
@@ -522,19 +533,22 @@ public class MetaTileEntityTKCYABlastFurnace extends RecipeMapMultiblockNoEnergy
         this.height = context.getOrDefault("blastFurnaceHeight", 1);
         this.recipeMapWorkable.setParallelLimit(height);
 
+        /*
         Object type = context.get("BrickType");
         if (type instanceof BlockBrick.BrickType) {
             this.targetTemp = ((BlockBrick.BrickType) type).getBrickTemperature();
-            this.texture = ((BlockBrick.BrickType) type).getTexture();
+            //this.texture = ((BlockBrick.BrickType) type).getTexture();
         } else
             this.targetTemp = BlockBrick.BrickType.BRICK.getBrickTemperature();
+
+         */
         //Particle
         //this.hasGasOutputHatchInt = context.getOrDefault("blastFurnaceHasGasOutput", 1);
     }
 
     @Override
     public MetaTileEntity createMetaTileEntity(IGregTechTileEntity tileEntity) {
-        return new MetaTileEntityTKCYABlastFurnace(metaTileEntityId);
+        return new MetaTileEntityTKCYABlastFurnace(metaTileEntityId, brick);
     }
 
     @Override
