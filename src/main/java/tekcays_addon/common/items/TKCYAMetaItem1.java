b40d8271b0cf14761be9745d2175b8c5bc4b116d
tekcay <pierreblc@hotmail.fr>
2022-08-09 19:49:23 +0200

    added missing textures and TextFormatting via setRarity

--- src/main/java/tekcays_addon/common/items/TKCYAMetaItem1.java
+++ src/main/java/tekcays_addon/common/items/TKCYAMetaItem1.java
@@ -1,6 +1,7 @@
 package tekcays_addon.common.items;
 
 import gregtech.api.items.metaitem.StandardMetaItem;
+import net.minecraft.item.EnumRarity;
 import tekcays_addon.api.items.metaitem.TraysStats;
 import tekcays_addon.common.items.behaviors.ElectrodeBehavior;
 
@@ -29,26 +30,26 @@ public class TKCYAMetaItem1 extends StandardMetaItem {
         BUBBLE_CAP_GALVANIZED_STEEL = addItem(6, "bubble_cap.galvanized_steel");
         BUBBLE_CAP_STAINLESS_STEEL = addItem(7, "bubble_cap.stainless_steel");
 
-        MICRO_BUBBLE_CAP_STEEL = addItem(8, "micro_bubble_cap.steel");
-        MICRO_BUBBLE_CAP_GALVANIZED_STEEL = addItem(9, "micro_bubble_cap.galvanized_steel");
-        MICRO_BUBBLE_CAP_STAINLESS_STEEL = addItem(10, "micro_bubble_cap.stainless_steel");
+        MICRO_BUBBLE_CAP_STEEL = addItem(8, "micro_bubble_cap.steel").setRarity(EnumRarity.UNCOMMON);
+        MICRO_BUBBLE_CAP_GALVANIZED_STEEL = addItem(9, "micro_bubble_cap.galvanized_steel").setRarity(EnumRarity.UNCOMMON);
+        MICRO_BUBBLE_CAP_STAINLESS_STEEL = addItem(10, "micro_bubble_cap.stainless_steel").setRarity(EnumRarity.UNCOMMON);
 
-        NANO_BUBBLE_CAP_STEEL = addItem(11, "nano_bubble_cap.steel");
-        NANO_BUBBLE_CAP_GALVANIZED_STEEL = addItem(12, "nano_bubble_cap.galvanized_steel");
-        NANO_BUBBLE_CAP_STAINLESS_STEEL = addItem(13, "nano_bubble_cap.stainless_steel");
+        NANO_BUBBLE_CAP_STEEL = addItem(11, "nano_bubble_cap.steel").setRarity(EnumRarity.RARE);
+        NANO_BUBBLE_CAP_GALVANIZED_STEEL = addItem(12, "nano_bubble_cap.galvanized_steel").setRarity(EnumRarity.RARE);
+        NANO_BUBBLE_CAP_STAINLESS_STEEL = addItem(13, "nano_bubble_cap.stainless_steel").setRarity(EnumRarity.RARE);
 
         //Distillation trays
         DISTILLATION_TRAY_STEEL = addItem(14, "distillation_tray.steel").addComponents(new TraysStats(Steel, 10, 100));
         DISTILLATION_TRAY_GALVANIZED_STEEL = addItem(15, "distillation_tray.galvanized_steel").addComponents(new TraysStats(GalvanizedSteel, 8, 100));
         DISTILLATION_TRAY_STAINLESS_STEEL = addItem(16, "distillation_tray.stainless_steel").addComponents(new TraysStats(StainlessSteel, 6, 100));
 
-        ADVANCED_DISTILLATION_TRAY_STEEL = addItem(20, "advanced_distillation_tray.steel").addComponents(new TraysStats(Steel, 5, 150));
-        ADVANCED_DISTILLATION_TRAY_GALVANIZED_STEEL = addItem(21, "advanced_distillation_tray.galvanized_steel").addComponents(new TraysStats(GalvanizedSteel, 8, 150));
-        ADVANCED_DISTILLATION_TRAY_STAINLESS_STEEL = addItem(22, "advanced_distillation_tray.stainless_steel").addComponents(new TraysStats(StainlessSteel, 6, 150));
+        ADVANCED_DISTILLATION_TRAY_STEEL = addItem(20, "advanced_distillation_tray.steel").addComponents(new TraysStats(Steel, 5, 150)).setRarity(EnumRarity.UNCOMMON);
+        ADVANCED_DISTILLATION_TRAY_GALVANIZED_STEEL = addItem(21, "advanced_distillation_tray.galvanized_steel").addComponents(new TraysStats(GalvanizedSteel, 8, 150)).setRarity(EnumRarity.UNCOMMON);
+        ADVANCED_DISTILLATION_TRAY_STAINLESS_STEEL = addItem(22, "advanced_distillation_tray.stainless_steel").addComponents(new TraysStats(StainlessSteel, 6, 150)).setRarity(EnumRarity.UNCOMMON);
 
-        DENSE_DISTILLATION_TRAY_STEEL = addItem(23, "dense_distillation_tray.steel").addComponents(new TraysStats(Steel, 5, 300));
-        DENSE_DISTILLATION_TRAY_GALVANIZED_STEEL = addItem(24, "dense_distillation_tray.galvanized_steel").addComponents(new TraysStats(GalvanizedSteel, 8, 300));
-        DENSE_DISTILLATION_TRAY_STAINLESS_STEEL = addItem(25, "dense_distillation_tray.stainless_steel").addComponents(new TraysStats(StainlessSteel, 6, 300));
+        DENSE_DISTILLATION_TRAY_STEEL = addItem(23, "dense_distillation_tray.steel").addComponents(new TraysStats(Steel, 5, 300)).setRarity(EnumRarity.RARE);
+        DENSE_DISTILLATION_TRAY_GALVANIZED_STEEL = addItem(24, "dense_distillation_tray.galvanized_steel").addComponents(new TraysStats(GalvanizedSteel, 8, 300)).setRarity(EnumRarity.RARE);
+        DENSE_DISTILLATION_TRAY_STAINLESS_STEEL = addItem(25, "dense_distillation_tray.stainless_steel").addComponents(new TraysStats(StainlessSteel, 6, 300)).setRarity(EnumRarity.RARE);
 
     }
 }
