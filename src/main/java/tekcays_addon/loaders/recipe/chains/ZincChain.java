67fa095f24e5ca4e508deb9f420bb9425e06b96f
tekcay <pierreblc@hotmail.fr>
2022-08-25 18:46:17 +0200

    replaced former GTCEu Zincite which no longer exist by GCYS's ZincOxide

--- src/main/java/tekcays_addon/loaders/recipe/chains/ZincChain.java
+++ src/main/java/tekcays_addon/loaders/recipe/chains/ZincChain.java
@@ -1,5 +1,6 @@
 package tekcays_addon.loaders.recipe.chains;
 
+import static gregicality.science.api.unification.materials.GCYSMaterials.ZincOxide;
 import static gregtech.api.recipes.RecipeMaps.CHEMICAL_RECIPES;
 import static gregtech.api.unification.material.Materials.*;
 import static gregtech.api.unification.ore.OrePrefix.*;
@@ -15,7 +16,7 @@ public class ZincChain {
 
         // ZnO + 2H2SO4 = ZnSO4 + ZincLeachingResidue [Contains: (H2O)(H2SO4)]
         CHEMICAL_RECIPES.recipeBuilder().duration(40).EUt(480)
-                .input(dust, Zincite)
+                .input(dust, ZincOxide)
                 .fluidInputs(SulfuricAcid.getFluid(2000))
                 .fluidOutputs(ZincLeachingSolution.getFluid(1000))
                 .buildAndRegister();
