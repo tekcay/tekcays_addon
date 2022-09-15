1182c7f7c71f8d50678fe760f4bd898496805bd5
tekcay <pierreblc@hotmail.fr>
2022-09-15 13:45:45 +0200

    added texture and model to the Dust Textures and several missing lang entries

---
+++ src/main/java/tekcays_addon/common/metatileentities/multi/MetaTileEntitySpiralSeparator.java
@@ -0,0 +1,136 @@
+package tekcays_addon.common.metatileentities.multi;
+
+import gregicality.science.api.metatileentity.multiblock.PressureMultiblockController;
+import gregicality.science.api.recipes.recipeproperties.PressureProperty;
+import gregtech.api.block.IHeatingCoilBlockStats;
+import gregtech.api.capability.impl.MultiblockRecipeLogic;
+import gregtech.api.metatileentity.MetaTileEntity;
+import gregtech.api.metatileentity.interfaces.IGregTechTileEntity;
+import gregtech.api.metatileentity.multiblock.IMultiblockPart;
+import gregtech.api.metatileentity.multiblock.RecipeMapMultiblockController;
+import gregtech.api.pattern.BlockPattern;
+import gregtech.api.pattern.FactoryBlockPattern;
+import gregtech.api.pattern.PatternMatchContext;
+import gregtech.api.recipes.Recipe;
+import gregtech.client.renderer.ICubeRenderer;
+import gregtech.client.renderer.texture.Textures;
+import gregtech.common.blocks.BlockMetalCasing;
+import gregtech.common.blocks.MetaBlocks;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.resources.I18n;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TextComponentTranslation;
+import net.minecraft.world.World;
+import tekcays_addon.api.recipes.TKCYARecipeMaps;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.util.List;
+
+public class MetaTileEntityPressurizedCrackingUnit extends PressureMultiblockController {
+
+    private int coilTier;
+
+    public MetaTileEntityPressurizedCrackingUnit(ResourceLocation metaTileEntityId) {
+        super(metaTileEntityId, TKCYARecipeMaps.PRESSURE_CRACKING);
+        this.recipeMapWorkable = new CrackingUnitWorkableHandler(this);
+    }
+
+    @Override
+    public MetaTileEntity createMetaTileEntity(IGregTechTileEntity tileEntity) {
+        return new MetaTileEntityPressurizedCrackingUnit(metaTileEntityId);
+    }
+
+    @Override
+    protected BlockPattern createStructurePattern() {
+        return FactoryBlockPattern.start()
+                .aisle("HCHCH", "HCHCH", "HCHCH")
+                .aisle("HCHCH", "H###H", "HCHCH")
+                .aisle("HCHCH", "HCOCH", "HCHCH")
+                .where('O', selfPredicate())
+                .where('H', states(getCasingState()).setMinGlobalLimited(12).or(autoAbilities()))
+                .where('#', air())
+                .where('C', heatingCoils())
+                .build();
+    }
+
+    @Override
+    public ICubeRenderer getBaseTexture(IMultiblockPart sourcePart) {
+        return Textures.CLEAN_STAINLESS_STEEL_CASING;
+    }
+
+    protected IBlockState getCasingState() {
+        return MetaBlocks.METAL_CASING.getState(BlockMetalCasing.MetalCasingType.STAINLESS_CLEAN);
+    }
+
+    @Override
+    protected void addDisplayText(List<ITextComponent> textList) {
+        super.addDisplayText(textList);
+        if (isStructureFormed()) {
+            textList.add(new TextComponentTranslation("gregtech.multiblock.cracking_unit.energy", 100 - 10 * coilTier));
+            double pressure = this.getPressureContainer().getPressure();
+            if (pressure > 100000) textList.add(new TextComponentTranslation("tkcya.machine.text.pressure", String.format("%.3f", pressure/1000D) + " kPa"));
+            if (pressure > 1000000) textList.add(new TextComponentTranslation("tkcya.machine.text.pressure", String.format("%.3f", pressure/1000000D) + " MPa"));
+        }
+    }
+
+    @Override
+    public void addInformation(ItemStack stack, @Nullable World player, List<String> tooltip, boolean advanced) {
+        super.addInformation(stack, player, tooltip, advanced);
+        tooltip.add(I18n.format("gregtech.machine.cracker.tooltip.1"));
+    }
+
+    @Override
+    public boolean checkRecipe(@Nonnull Recipe recipe, boolean consumeIfSuccess) {
+        return this.getPressureContainer().getPressure() >= recipe.getProperty(PressureProperty.getInstance(), 0D);
+        //TODO : to fill with temperature checking when it is implemented
+    }
+
+    @Nonnull
+    @Override
+    protected ICubeRenderer getFrontOverlay() {
+        return Textures.CRACKING_UNIT_OVERLAY;
+    }
+
+    @Override
+    protected void formStructure(PatternMatchContext context) {
+        super.formStructure(context);
+        Object type = context.get("CoilType");
+        if (type instanceof IHeatingCoilBlockStats) {
+            this.coilTier = ((IHeatingCoilBlockStats) type).getTier();
+        } else {
+            this.coilTier = 0;
+        }
+    }
+
+    @Override
+    public void invalidateStructure() {
+        super.invalidateStructure();
+        this.coilTier = -1;
+    }
+
+    protected int getCoilTier() {
+        return this.coilTier;
+    }
+
+    @SuppressWarnings("InnerClassMayBeStatic")
+    private class CrackingUnitWorkableHandler extends MultiblockRecipeLogic {
+
+        public CrackingUnitWorkableHandler(RecipeMapMultiblockController tileEntity) {
+            super(tileEntity);
+        }
+
+        @Override
+        protected void performNonOverclockBonuses(int[] resultOverclock) {
+
+            int coilTier = ((MetaTileEntityPressurizedCrackingUnit) metaTileEntity).getCoilTier();
+            if (coilTier <= 0)
+                return;
+
+            resultOverclock[0] *= 1.0f - coilTier * 0.1; // each coil above cupronickel (coilTier = 0) uses 10% less energy
+            resultOverclock[0] = Math.max(1, resultOverclock[0]);
+        }
+    }
+}
