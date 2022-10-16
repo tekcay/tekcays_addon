cd7fac64f297de50ad098dba1da9de1dbb3ff83c
tekcay <pierreblc@hotmail.fr>
2022-10-16 12:27:45 +0200

    Fluidized Heaters now consume coke as well

--- src/main/java/tekcays_addon/common/metatileentities/single/MetaTileEntityFluidizedHeater.java
+++ src/main/java/tekcays_addon/common/metatileentities/single/MetaTileEntityFluidizedHeater.java
@@ -4,22 +4,29 @@ import codechicken.lib.render.CCRenderState;
 import codechicken.lib.render.pipeline.IVertexOperation;
 import codechicken.lib.vec.Matrix4;
 import gregtech.api.capability.IActiveOutputSide;
-import gregtech.api.capability.impl.FilteredFluidHandler;
 import gregtech.api.capability.impl.FluidTankList;
+import gregtech.api.capability.impl.NotifiableItemStackHandler;
 import gregtech.api.gui.GuiTextures;
 import gregtech.api.gui.ModularUI;
 import gregtech.api.gui.widgets.LabelWidget;
+import gregtech.api.gui.widgets.SlotWidget;
 import gregtech.api.gui.widgets.TankWidget;
 import gregtech.api.metatileentity.IDataInfoProvider;
 import gregtech.api.metatileentity.MetaTileEntity;
 import gregtech.api.metatileentity.interfaces.IGregTechTileEntity;
+import gregtech.api.unification.OreDictUnifier;
+import gregtech.api.unification.ore.OrePrefix;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.network.PacketBuffer;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.World;
 import net.minecraftforge.fluids.FluidTank;
+import net.minecraftforge.fluids.IFluidTank;
+import net.minecraftforge.items.IItemHandlerModifiable;
+import net.minecraftforge.items.ItemStackHandler;
 import tekcays_addon.api.capability.impl.HeatContainer;
 import tekcays_addon.api.metatileentity.FuelHeater;
 import tekcays_addon.api.render.TKCYATextures;
@@ -28,13 +35,14 @@ import javax.annotation.Nullable;
 import java.util.List;
 
 import static gregtech.api.capability.GregtechDataCodes.IS_WORKING;
-import static gregtech.api.unification.material.Materials.Calcite;
+import static gregtech.api.unification.material.Materials.Coke;
 import static tekcays_addon.api.utils.FuelWithProperties.CALCITE;
 import static tekcays_addon.api.utils.HeatersMethods.getBurnTime;
 
 public class MetaTileEntityFluidizedHeater extends FuelHeater implements IDataInfoProvider, IActiveOutputSide {
 
-    private FluidTank fuelFluidTank;
+    private final Item SOLID_FUEL = OreDictUnifier.get(OrePrefix.dustTiny, Coke).getItem();
+    private final int CALCITE_AMOUNT = CALCITE.getFluidStack().amount;
 
     public MetaTileEntityFluidizedHeater(ResourceLocation metaTileEntityId, tekcays_addon.api.utils.FuelHeater fuelHeater) {
         super(metaTileEntityId, fuelHeater);
@@ -51,16 +59,23 @@ public class MetaTileEntityFluidizedHeater extends FuelHeater implements IDataIn
     protected void initializeInventory() {
         super.initializeInventory();
         this.importFluids = this.createImportFluidHandler();
+        this.importItems = this.createImportItemHandler();
+        this.exportItems = this.createExportItemHandler();
         this.heatContainer = new HeatContainer(this, 0, 20000);
     }
 
+    @Override
+    protected IItemHandlerModifiable createImportItemHandler() {
+        return new NotifiableItemStackHandler(1, this, false);
+    }
+    @Override
+    protected IItemHandlerModifiable createExportItemHandler() {
+        return new ItemStackHandler(1);
+    }
 
     @Override
     protected FluidTankList createImportFluidHandler() {
-        FluidTankList superHandler = super.createImportFluidHandler();
-        this.fuelFluidTank = new FilteredFluidHandler(1000);
-        fuelFluidTank.setFluid(Calcite.getFluid(0));
-        return new FluidTankList(false, superHandler, fuelFluidTank);
+        return new FluidTankList(false, new FluidTank(1000));
     }
 
     @Override
@@ -72,6 +87,10 @@ public class MetaTileEntityFluidizedHeater extends FuelHeater implements IDataIn
         return ModularUI.builder(GuiTextures.BACKGROUND, 176, 166)
                 .shouldColor(false)
                 .widget(new LabelWidget(5, 5, getMetaFullName()))
+                .widget(new SlotWidget(importItems, 0, 50, 10, true, true)
+                        .setBackgroundTexture(GuiTextures.SLOT))
+                .widget(new SlotWidget(exportItems, 0, 50, 60, true, false)
+                        .setBackgroundTexture(GuiTextures.SLOT))
                 .widget(new TankWidget(importFluids.getTankAt(0), 20, 50, 18, 18)
                         .setBackgroundTexture(GuiTextures.FLUID_SLOT)
                         .setAlwaysShowFull(true)
@@ -86,12 +105,22 @@ public class MetaTileEntityFluidizedHeater extends FuelHeater implements IDataIn
         TKCYATextures.FLUIDIZED_FUEL_HEATER.renderOrientedState(renderState, translation, pipeline, getFrontFacing(), isBurning(), true);
     }
 
+    private boolean isThereEnoughCalcite(IFluidTank fuelFluidTank) {
+        return fuelFluidTank.getFluid() != null &&
+                fuelFluidTank.getFluid().isFluidEqual(CALCITE.getFluidStack()) &&
+                fuelFluidTank.getFluidAmount() >= CALCITE_AMOUNT;
+    }
+
+    private boolean isThereCoke() {
+        return importItems.getStackInSlot(0).getItem().equals(SOLID_FUEL);
+    }
+
     @Override
     protected void tryConsumeNewFuel() {
-        if (fuelFluidTank.getFluid() != null &&
-                fuelFluidTank.getFluid().isFluidEqual(CALCITE.getFluidStack()) &&
-                        fuelFluidTank.getFluidAmount() >= CALCITE.getFluidStack().amount) {
-            fuelFluidTank.drain(CALCITE.getFluidStack(), true);
+        IFluidTank fuelFluidTank = importFluids.getTankAt(0);
+        if (isThereEnoughCalcite(fuelFluidTank) && isThereCoke()) {
+            fuelFluidTank.drain(CALCITE_AMOUNT, true);
+            importItems.extractItem(0, 1, false);
             setBurnTimeLeft(getBurnTime(CALCITE, fuelHeater));
         }
     }
