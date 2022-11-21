2a7267766fcd708c8051a63ed30587148fedd317
tekcay <pierreblc@hotmail.fr>
2022-11-21 17:45:17 +0100

    added a method that tweaks the chemical formula of several atomic elements

---
+++ src/main/java/tekcays_addon/api/unification/material/info/TKCYAChemicalFormula.java
@@ -0,0 +1,18 @@
+package tekcays_addon.api.unification.material.info;
+
+import gregtech.api.unification.material.Materials;
+
+public class TKCYAChemicalFormula {
+
+    public static void modifyChemicalFormula() {
+        Materials.Oxygen.setFormula("O2", true);
+        Materials.LiquidOxygen.setFormula("O2", true);
+        Materials.Hydrogen.setFormula("H2", true);
+        Materials.Nitrogen.setFormula("N2", true);
+        Materials.Sulfur.setFormula("S8", true);
+        Materials.Fluorine.setFormula("F2", true);
+        Materials.Chlorine.setFormula("Cl2", true);
+        Materials.Bromine.setFormula("Br2", true);
+        Materials.Iodine.setFormula("I2", true);
+    }
+}
