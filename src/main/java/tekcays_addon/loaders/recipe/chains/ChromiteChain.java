f17b9904681c55d06af65dc1e2f49ac634478417
tekcay <pierreblc@hotmail.fr>
2022-12-04 15:44:11 +0100

    added recipes to Advanced Blast Furnace

--- src/main/java/tekcays_addon/loaders/recipe/chains/ChromiteChain.java
+++ src/main/java/tekcays_addon/loaders/recipe/chains/ChromiteChain.java
@@ -5,10 +5,11 @@ import tekcays_addon.common.items.TKCYAMetaItems;
 
 import static gregtech.api.GTValues.MV;
 import static gregtech.api.GTValues.VA;
-import static gregtech.api.recipes.RecipeMaps.BLAST_RECIPES;
 import static gregtech.api.recipes.RecipeMaps.CHEMICAL_RECIPES;
+import static gregtech.api.recipes.RecipeMaps.LARGE_CHEMICAL_RECIPES;
 import static gregtech.api.unification.material.Materials.*;
 import static gregtech.api.unification.ore.OrePrefix.dust;
+import static tekcays_addon.api.recipes.TKCYARecipeMaps.ADVANCED_BLAST_FURNACE_RECIPES;
 import static tekcays_addon.api.unification.TKCYAMaterials.*;
 
 public class ChromiteChain {
@@ -26,7 +27,7 @@ public class ChromiteChain {
                 .EUt(VA[MV])
                 .buildAndRegister();
 
-        CHEMICAL_RECIPES.recipeBuilder()
+        LARGE_CHEMICAL_RECIPES.recipeBuilder()
                 .notConsumable(TKCYAMetaItems.GAS_COLLECTOR.getStackForm())
                 .input(dust, Chromite, 14)
                 .input(dust, SodiumCarbonate, 12)
@@ -50,36 +51,23 @@ public class ChromiteChain {
                 .buildAndRegister();
 
         //STEP 3: 2 Na2CrO4 + C -> Cr2O3 + 2 Na2CO3 + CO
-        BLAST_RECIPES.recipeBuilder()
+        ADVANCED_BLAST_FURNACE_RECIPES.recipeBuilder()
                 .input(dust, SodiumDichromate, 11)
                 .input(dust, Carbon, 2)
                 .output(dust, ChromiumOxide, 8)
                 .output(dust, SodiumCarbonate, 6)
-                .blastFurnaceTemp(2000)
+                .temperature(2000)
                 .duration(80)
-                .EUt(VA[MV])
-                .buildAndRegister();
-
-        BLAST_RECIPES.recipeBuilder()
-                .input(dust, SodiumDichromate, 11)
-                .input(dust, Carbon, 2)
-                .output(dust, ChromiumOxide, 8)
-                .output(dust, SodiumCarbonate, 6)
-                .notConsumable(TKCYAMetaItems.GAS_COLLECTOR.getStackForm())
-                .fluidOutputs(CarbonMonoxide.getFluid(2000))
-                .blastFurnaceTemp(2000)
-                .duration(80)
-                .EUt(VA[MV])
                 .buildAndRegister();
 
         //STEP 4: Cr2O3 + 2 Al -> Al2O3 + 2 Cr
-        CHEMICAL_RECIPES.recipeBuilder()
+        ADVANCED_BLAST_FURNACE_RECIPES.recipeBuilder()
                 .input(dust, ChromiumOxide, 5)
                 .input(dust, Aluminium, 2)
                 .output(dust, Alumina, 5)
                 .fluidOutputs(Chrome.getFluid(GTValues.L * 2))
                 .duration(80)
-                .EUt(VA[MV])
+                .temperature(Chrome.getFluid().getTemperature())
                 .buildAndRegister();
     }
 
