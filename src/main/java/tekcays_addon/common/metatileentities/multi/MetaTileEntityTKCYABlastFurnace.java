64e92236e29582706f421aca96de8807f26724ae
tekcay <pierreblc@hotmail.fr>
2022-12-05 20:15:36 +0100

    tweaked the PBF structure, removed parallel abilities

--- src/main/java/tekcays_addon/common/metatileentities/multi/MetaTileEntityTKCYABlastFurnace.java
+++ src/main/java/tekcays_addon/common/metatileentities/multi/MetaTileEntityTKCYABlastFurnace.java
@@ -5,17 +5,12 @@ import gregtech.api.metatileentity.interfaces.IGregTechTileEntity;
 import gregtech.api.metatileentity.multiblock.*;
 import gregtech.api.pattern.BlockPattern;
 import gregtech.api.pattern.FactoryBlockPattern;
-import gregtech.api.pattern.PatternMatchContext;
-import gregtech.api.pattern.TraceabilityPredicate;
 import gregtech.api.recipes.Recipe;
 import gregtech.client.renderer.ICubeRenderer;
 import gregtech.client.renderer.texture.Textures;
-import gregtech.common.metatileentities.MetaTileEntities;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.network.PacketBuffer;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.Style;
@@ -23,7 +18,6 @@ import net.minecraft.util.text.TextComponentTranslation;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.util.text.event.HoverEvent;
 import net.minecraft.world.World;
-import tekcays_addon.api.capability.impl.NoEnergyMultiblockLogic;
 import tekcays_addon.api.metatileentity.multiblock.NoEnergyRecipeMapMultiBlockController;
 import tekcays_addon.api.recipes.TKCYARecipeMaps;
 import tekcays_addon.api.recipes.recipeproperties.NoEnergyTemperatureProperty;
@@ -43,14 +37,11 @@ public class MetaTileEntityTKCYABlastFurnace extends NoEnergyRecipeMapMultiBlock
 
     private final BlockBrick.BrickType brick;
     private final IBlockState iBlockState;
-    private final int PARALLEL_MULTIPLIER = 2;
-    public int height;
 
     public MetaTileEntityTKCYABlastFurnace(ResourceLocation metaTileEntityId, BlockBrick.BrickType brick) {
         super(metaTileEntityId, TKCYARecipeMaps.BLASTING_RECIPES);
         this.brick = brick;
         this.iBlockState = TKCYAMetaBlocks.BLOCK_BRICK.getState(brick);
-        this.recipeMapWorkable = new NoEnergyMultiblockLogic(this);
     }
 
     @Override
@@ -61,8 +52,6 @@ public class MetaTileEntityTKCYABlastFurnace extends NoEnergyRecipeMapMultiBlock
     public void addInformation(@Nonnull ItemStack stack, @Nullable World player, @Nonnull List<String> tooltip, boolean advanced) {
         super.addInformation(stack, player, tooltip, advanced);
         tooltip.add(I18n.format("tekcays_addon.machine.tkcya_blast_furnace.tooltip.1"));
-        tooltip.add(I18n.format("tkcya.machine.parallel_ability.tooltip"));
-        tooltip.add(I18n.format("tekcays_addon.machine.tkcya_blast_furnace.tooltip.3", PARALLEL_MULTIPLIER));
         tooltip.add(I18n.format("tekcays_addon.machine.tkcya_blast_furnace.tooltip.4", brick.getBrickTemperature()));
     }
 
@@ -74,9 +63,6 @@ public class MetaTileEntityTKCYABlastFurnace extends NoEnergyRecipeMapMultiBlock
             textList.add((new TextComponentTranslation("gregtech.multiblock.invalid_structure"))
                     .setStyle((new Style()).setColor(TextFormatting.RED).setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, tooltip))));
         } else {
-            if (this.recipeMapWorkable.getParallelLimit() != 1) {
-                textList.add(new TextComponentTranslation("gregtech.multiblock.parallel", this.recipeMapWorkable.getParallelLimit()));
-            }
             if (!this.recipeMapWorkable.isWorkingEnabled()) {
                 textList.add(new TextComponentTranslation("gregtech.multiblock.work_paused"));
             } else if (this.recipeMapWorkable.isActive()) {
@@ -122,59 +108,19 @@ public class MetaTileEntityTKCYABlastFurnace extends NoEnergyRecipeMapMultiBlock
     @Override
     protected BlockPattern createStructurePattern() {
         return FactoryBlockPattern.start(RIGHT, FRONT, UP)
-                .aisle("#YYY#", "YXXXY", "YXXXY", "YXXXY", "#YYY#")
-                .aisle("#YSY#", "Y###Y", "Y###Y", "Y###Y", "#YYY#")
-                .aisle("#YYY#", "Y###Y", "Y#H#Y", "Y###Y", "#YYY#").setRepeatable(1, 11)
-                .aisle("#YYY#", "YOOOY", "YOMOY", "YOOOY", "#YYY#")
+                .aisle("YYY", "YOY", "YYY")
+                .aisle("YSY", "Y#Y", "YYY")
+                .aisle("AIA", "IAI", "AIA")
+                .aisle("AAA", "AMA", "AAA")
                 .where('S', selfPredicate())
                 .where('Y', states(getCasingState()))
-                .where('X', states(getCasingState())
-                        .or(getOutputBrickFluidHatch(brick).setMinGlobalLimited(1).setMaxGlobalLimited(1)))
-                .where('O', states(getCasingState())
-                        .or(getInputBrickItemBus(brick).setMinGlobalLimited(1).setMaxGlobalLimited(2)))
-                .where('H', heightIndicatorPredicate())
-                .where('M', metaTileEntities(MetaTileEntities.MUFFLER_HATCH)
-                        .or(getBrickMuffler(brick)))
+                .where('O', getOutputBrickFluidHatch(brick).setMinGlobalLimited(1).setMaxGlobalLimited(1))
+                .where('I', getInputBrickItemBus(brick).setMinGlobalLimited(1).setMaxGlobalLimited(2)
+                        .or(states(getCasingState())))
+                .where('M', getBrickMuffler(brick))
                 .where('#', air())
+                .where('A', any())
                 .build();
-    }
-
-    @Override
-    public void readFromNBT(NBTTagCompound data) {
-        super.readFromNBT(data);
-        this.height = data.getInteger("height");
-    }
-
-    @Override
-    public void writeInitialSyncData(PacketBuffer buf) {
-        super.writeInitialSyncData(buf);
-        buf.writeInt(this.height);
-    }
-
-    @Override
-    public void receiveInitialSyncData(PacketBuffer buf) {
-        super.receiveInitialSyncData(buf);
-        this.height = buf.readInt();
-    }
-
-
-    // This function is highly useful for detecting the length of this multiblock.
-    public static TraceabilityPredicate heightIndicatorPredicate() {
-        return new TraceabilityPredicate((blockWorldState) -> {
-            if (air().test(blockWorldState)) {
-                blockWorldState.getMatchContext().increment("blastFurnaceHeight", 1);
-                return true;
-            } else
-                return false;
-        });
-    }
-
-    @Override
-    protected void formStructure(PatternMatchContext context) {
-        super.formStructure(context);
-        initializeAbilities();
-        this.height = context.getOrDefault("blastFurnaceHeight", 1);
-        this.recipeMapWorkable.setParallelLimit(height < 2 ? 1 : height * 2);
     }
 
     @Override
