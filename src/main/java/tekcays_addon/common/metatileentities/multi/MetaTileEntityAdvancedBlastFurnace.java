95dbe43c29d571e3ba8184f8e90ad35194038c7a
tekcay <pierreblc@hotmail.fr>
2022-12-06 20:18:15 +0100

    heatContainer is now a HeatContainerList, taking into account all the HeatAcceptors of the multi

--- src/main/java/tekcays_addon/common/metatileentities/multi/MetaTileEntityAdvancedBlastFurnace.java
+++ src/main/java/tekcays_addon/common/metatileentities/multi/MetaTileEntityAdvancedBlastFurnace.java
@@ -30,6 +30,7 @@ import net.minecraft.world.World;
 import org.lwjgl.input.Keyboard;
 import tekcays_addon.api.capability.IHeatContainer;
 import tekcays_addon.api.capability.IHeatMachine;
+import tekcays_addon.api.capability.impl.HeatContainerList;
 import tekcays_addon.api.metatileentity.multiblock.HeatContainerNoEnergyMultiblockController;
 import tekcays_addon.api.metatileentity.multiblock.TKCYAMultiblockAbility;
 import tekcays_addon.api.recipes.recipeproperties.NoEnergyTemperatureProperty;
@@ -182,7 +183,7 @@ public class MetaTileEntityAdvancedBlastFurnace extends HeatContainerNoEnergyMul
     }
 
     private void updateLogic() {
-        heatContainer = getAbilities(TKCYAMultiblockAbility.HEAT_CONTAINER).get(0);
+        heatContainer = new HeatContainerList(getAbilities(TKCYAMultiblockAbility.HEAT_CONTAINER));
 
         currentTemp = heatContainer.getTemperature();
         currentHeat = heatContainer.getHeat();
