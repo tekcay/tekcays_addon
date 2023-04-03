aeafc4635161b0fee52190d9893ae49d6e0e166a
Pierre <pierreblc@hotmail.fr>
2023-04-03 22:49:32 +0200

    integrated PressureControl capability to HeatedPressure multiblock type, and reworked a bit the containers initialization

---
+++ src/main/java/tekcays_addon/api/capability/IPressureControlMachine.java
@@ -0,0 +1,9 @@
+package tekcays_addon.api.capability;
+
+public interface IPressureControlMachine {
+
+    /**
+     * @return the pressureControl of this machine
+     */
+    IPressureControl getPressureControl();
+}
--- src/main/java/tekcays_addon/api/metatileentity/multiblock/HeatedPressureContainerMultiblockController.java
+++ src/main/java/tekcays_addon/api/metatileentity/multiblock/HeatedPressureContainerMultiblockController.java
@@ -5,18 +5,18 @@ import gregtech.api.metatileentity.multiblock.RecipeMapMultiblockController;
 import gregtech.api.pattern.TraceabilityPredicate;
 import gregtech.api.recipes.RecipeMap;
 import net.minecraft.util.ResourceLocation;
-import tekcays_addon.api.capability.IHeatContainer;
-import tekcays_addon.api.capability.IHeatMachine;
-import tekcays_addon.api.capability.IPressureContainer;
-import tekcays_addon.api.capability.IPressureMachine;
+import tekcays_addon.api.capability.*;
 import tekcays_addon.api.capability.impl.*;
 
 import java.util.List;
 
-public abstract class HeatedPressureContainerMultiblockController extends RecipeMapMultiblockController implements IPressureMachine, IHeatMachine {
+import static tekcays_addon.api.metatileentity.multiblock.TKCYAMultiblockAbility.*;
+
+public abstract class HeatedPressureContainerMultiblockController extends RecipeMapMultiblockController implements IPressureMachine, IHeatMachine, IPressureControlMachine {
 
     private IPressureContainer pressureContainer;
     private IHeatContainer heatContainer;
+    private IPressureControl pressureControl;
 
     public HeatedPressureContainerMultiblockController(ResourceLocation metaTileEntityId, RecipeMap<?> recipeMap) {
         super(metaTileEntityId, recipeMap);
@@ -26,11 +26,14 @@ public abstract class HeatedPressureContainerMultiblockController extends Recipe
     @Override
     protected void initializeAbilities() {
         super.initializeAbilities();
-        this.heatContainer = new HeatContainerList(getAbilities(TKCYAMultiblockAbility.HEAT_CONTAINER));
-        List<IPressureContainer> list = getAbilities(TKCYAMultiblockAbility.PRESSURE_CONTAINER);
-        this.pressureContainer = list.isEmpty() ? null : list.get(0);
-    }
+        this.heatContainer = new HeatContainerList(getAbilities(HEAT_CONTAINER));
 
+        List<IPressureContainer> list = getAbilities(PRESSURE_CONTAINER);
+        this.pressureContainer = list.isEmpty() ? null : list.get(0);
+
+        List<IPressureControl> list2 = getAbilities(PRESSURE_CONTROL);
+        this.pressureControl = list2.isEmpty() ? null : list2.get(0);
+    }
 
     @Override
     public void invalidateStructure() {
@@ -40,7 +43,9 @@ public abstract class HeatedPressureContainerMultiblockController extends Recipe
     @Override
     public TraceabilityPredicate autoAbilities(boolean checkEnergyIn, boolean checkMaintenance, boolean checkItemIn, boolean checkItemOut, boolean checkFluidIn, boolean checkFluidOut, boolean checkMuffler) {
         TraceabilityPredicate predicate = super.autoAbilities(checkEnergyIn, checkMaintenance, checkItemIn, checkItemOut, checkFluidIn, checkFluidOut, checkMuffler);
-        predicate = predicate.or(abilities(TKCYAMultiblockAbility.PRESSURE_CONTAINER)).setMaxGlobalLimited(1);
+        predicate = predicate
+                .or(abilities(PRESSURE_CONTAINER)).setExactLimit(1)
+                .or(abilities(PRESSURE_CONTROL)).setExactLimit(1);
         return predicate;
     }
 
@@ -52,5 +57,10 @@ public abstract class HeatedPressureContainerMultiblockController extends Recipe
     @Override
     public IHeatContainer getHeatContainer() {
         return this.heatContainer;
+    }
+
+    @Override
+    public IPressureControl getPressureControl() {
+        return this.pressureControl;
     }
 }
