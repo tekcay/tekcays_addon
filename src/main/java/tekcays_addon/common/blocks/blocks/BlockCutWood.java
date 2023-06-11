e83cf909dc4b92934d4c9b1062833a2d83908821
Pierre <pierreblc@hotmail.fr>
2023-06-11 22:51:00 +0200

    added logic to turn cutWoot into planks then into slab

--- src/main/java/tekcays_addon/common/blocks/blocks/BlockCutWood.java
+++ src/main/java/tekcays_addon/common/blocks/blocks/BlockCutWood.java
@@ -2,41 +2,151 @@ package tekcays_addon.common.blocks.blocks;
 
 import gregtech.api.block.VariantBlock;
 import gregtech.api.block.VariantItemBlock;
-import gregtech.client.renderer.texture.cube.SimpleOverlayRenderer;
+import gregtech.common.blocks.MetaBlocks;
 import lombok.Getter;
-import net.minecraft.block.BlockNewLog;
 import net.minecraft.block.BlockOldLog;
 import net.minecraft.block.BlockPlanks;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
-import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.IStringSerializable;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
-import tekcays_addon.gtapi.render.TKCYATextures;
+import tekcays_addon.common.blocks.TKCYAMetaBlocks;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
+import java.util.Arrays;
 import java.util.List;
-
-import static net.minecraft.init.Blocks.LOG;
-import static net.minecraft.init.Blocks.LOG2;
 
 public class BlockCutWood extends VariantBlock<BlockCutWood.CutWoodType> {
 
 
+    public static final String TRANSLATION_KEY = "block_cut_wood";
+    public static final String LOG = "log";
+    public static final String PLANK = "planks";
+
     public BlockCutWood() {
         super(Material.WOOD);
-        setTranslationKey("block_cut_wood");
+        setTranslationKey(TRANSLATION_KEY);
         setHardness(5.0f);
         setResistance(10.0f);
         setSoundType(SoundType.WOOD);
         setHarvestLevel("axe", 1);
         setDefaultState(getState(CutWoodType.OAK));
+    }
+
+    @Nullable
+    public static CutWoodType getCutWoodTypeFromLog(IBlockState block) {
+
+        if (block.equals(Blocks.LOG2.getDefaultState())) {
+            return CutWoodType.ACACIA;
+
+        } else if (block.equals(MetaBlocks.RUBBER_LOG.getDefaultState())) {
+            return CutWoodType.RUBBER;
+
+        } else if (block.equals(getLog(BlockPlanks.EnumType.OAK))) {
+            return CutWoodType.OAK;
+
+        } else if (block.equals(getLog(BlockPlanks.EnumType.BIRCH))) {
+            return CutWoodType.BIRCH;
+
+        } else if (block.equals(getLog(BlockPlanks.EnumType.SPRUCE))) {
+            return CutWoodType.SPRUCE;
+
+        } else if (block.equals(getLog(BlockPlanks.EnumType.JUNGLE))) {
+            return CutWoodType.JUNGLE;
+        }
+
+        return null;
+    }
+
+
+    @Nullable
+    public static IBlockState getPlankFromCutWood(IBlockState block) {
+        CutWoodType cutWoodType = getCustomType(block);
+        return cutWoodType == null ? null : getPlank(cutWoodType.getVariant());
+    }
+
+    @Nullable
+    public static IBlockState getSlabFromPlank(IBlockState block) {
+        CutWoodType cutWoodType = getCustomType(block);
+        return cutWoodType == null ? null : getSlab(cutWoodType.getVariant());
+    }
+
+    public static CutWoodType getCustomType(IBlockState blockState) {
+        String name = blockState.toString().split("=")[1].split("]")[0];
+        return getCustomType(name);
+    }
+
+    public static CutWoodType getCustomType(String name) {
+        return Arrays.stream(CutWoodType.values())
+                .filter(cutWoodType -> cutWoodType.getName().equals(name))
+                .findFirst()
+                .orElse(null);
+    }
+
+    public static CutWoodType getCustomType(BlockPlanks.EnumType variant) {
+        return Arrays.stream(CutWoodType.values())
+                .filter(cutWoodType -> cutWoodType.getVariant().equals(variant))
+                .findFirst()
+                .orElse(null);
+    }
+
+    /**
+     * 0 is blockType, 1 is woodType
+     *
+     * @param blockState
+     * @return
+     */
+    public static String[] getInfo(IBlockState blockState) {
+        String intermediate = blockState.toString().split(":")[1];
+        return intermediate.split("\\[");
+    }
+
+    public static @Nullable IBlockState getUpperBlockBlockState(String[] infos) {
+        CutWoodType cutWoodType = getCustomType(infos[1]);
+        switch (infos[0]) {
+            case LOG : {
+                return getLog(cutWoodType.getVariant());
+            }
+            case TRANSLATION_KEY : {
+                return getCutLog(cutWoodType.getVariant());
+            }
+            case PLANK : {
+                return getPlank(cutWoodType.getVariant());
+            }
+            default : return null;
+        }
+    }
+
+    /*
+    public static @Nullable String getBlockType(IBlockState blockState) {
+        String[] infos = blockState.toString().split(":")[1].split("\\[");
+        CutWoodType cutWoodType = getCustomType(infos[1]);
+        switch (infos[0]) {
+            case LOG : return getLog(cutWoodType.getVariant());
+            case TRANSLATION_KEY : return getCutLog(cutWoodType.getVariant());
+            case PLANK : return getPlank(cutWoodType.getVariant());
+            default : return null;
+        }
+    }
+
+     */
+
+    public static @Nullable IBlockState getUpperBlockBlockState(IBlockState blockState) {
+        String[] infos = blockState.toString().split(":")[1].split("\\[");
+        CutWoodType cutWoodType = getCustomType(infos[1]);
+        switch (infos[0]) {
+            case LOG : return getLog(cutWoodType.getVariant());
+            case TRANSLATION_KEY : return getCutLog(cutWoodType.getVariant());
+            case PLANK : return getPlank(cutWoodType.getVariant());
+            default : return null;
+        }
     }
 
     @Override
@@ -50,22 +160,42 @@ public class BlockCutWood extends VariantBlock<BlockCutWood.CutWoodType> {
         CutWoodType cutWoodType = getState(stackState);
     }
 
+    private static IBlockState getPlank(@Nullable BlockPlanks.EnumType plank) {
+        if (plank == null) return MetaBlocks.PLANKS.getDefaultState();
+        return Blocks.PLANKS.getDefaultState().withProperty(BlockPlanks.VARIANT, plank);
+    }
+
+    private static IBlockState getSlab(@Nullable BlockPlanks.EnumType plank) {
+        if (plank == null) return MetaBlocks.WOOD_SLAB.getDefaultState();
+        return Blocks.WOODEN_SLAB.getDefaultState().withProperty(BlockPlanks.VARIANT, plank);
+    }
+
+    private static IBlockState getLog(@Nullable BlockPlanks.EnumType plank) {
+        if (plank == null) return MetaBlocks.RUBBER_LOG.getDefaultState();
+        return Blocks.LOG.getDefaultState().withProperty(BlockOldLog.VARIANT, plank);
+    }
+
+    private static IBlockState getCutLog(@Nullable BlockPlanks.EnumType plank) {
+        if (plank == null) return MetaBlocks.RUBBER_LOG.getDefaultState();
+        return TKCYAMetaBlocks.BLOCK_CUT_WOOD.getState(getCustomType(plank));
+    }
 
     @Getter
     public enum CutWoodType implements IStringSerializable {
 
-        ACACIA("acacia"),
-        BIRCH("birch"),
-        JUNGLE("jungle"),
-        OAK("oak"),
-        RUBBER("rubber"),
-        SPRUCE("spruce");
+        ACACIA("acacia", BlockPlanks.EnumType.ACACIA),
+        BIRCH("birch", BlockPlanks.EnumType.BIRCH),
+        JUNGLE("jungle", BlockPlanks.EnumType.JUNGLE),
+        OAK("oak", BlockPlanks.EnumType.OAK),
+        RUBBER("rubber", null),
+        SPRUCE("spruce", BlockPlanks.EnumType.SPRUCE);
 
         private final String name;
+        private final BlockPlanks.EnumType variant;
 
-
-        CutWoodType(String name) {
+        CutWoodType(String name, BlockPlanks.EnumType variant) {
             this.name = name;
+            this.variant = variant;
         }
     }
 }
--- src/main/java/tekcays_addon/common/metatileentities/primitive/MetaTileEntityAxeSupport.java
+++ src/main/java/tekcays_addon/common/metatileentities/primitive/MetaTileEntityAxeSupport.java
@@ -5,62 +5,44 @@ import codechicken.lib.render.CCRenderState;
 import codechicken.lib.render.pipeline.ColourMultiplier;
 import codechicken.lib.render.pipeline.IVertexOperation;
 import codechicken.lib.vec.Matrix4;
-import com.ibm.icu.impl.data.HolidayBundle_it_IT;
 import gregtech.api.gui.ModularUI;
-import gregtech.api.items.toolitem.ItemGTAxe;
 import gregtech.api.items.toolitem.ToolClasses;
 import gregtech.api.items.toolitem.ToolHelper;
 import gregtech.api.metatileentity.MetaTileEntity;
 import gregtech.api.metatileentity.interfaces.IGregTechTileEntity;
 import gregtech.api.util.GTUtility;
 import gregtech.client.renderer.texture.cube.SimpleOverlayRenderer;
-import gregtech.common.blocks.MetaBlocks;
-import gregtech.common.items.MetaItems;
-import gregtech.common.items.ToolItems;
-import gregtech.core.sound.GTSoundEvents;
-import gregtech.tools.ToolsModule;
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockNewLog;
-import net.minecraft.block.BlockOldLog;
-import net.minecraft.block.BlockPlanks;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
-import net.minecraft.init.SoundEvents;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemAxe;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.network.PacketBuffer;
-import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.ResourceLocation;
-import net.minecraft.util.SoundEvent;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 import org.apache.commons.lang3.ArrayUtils;
 import tekcays_addon.common.blocks.TKCYAMetaBlocks;
-import tekcays_addon.common.blocks.blocks.BlockBrick;
 import tekcays_addon.common.blocks.blocks.BlockCutWood;
 import tekcays_addon.gtapi.render.TKCYATextures;
-import tekcays_addon.gtapi.utils.TKCYALog;
 
 import java.util.Set;
 
-import static gregtech.api.capability.GregtechDataCodes.IS_WORKING;
-import static net.minecraft.util.EnumFacing.DOWN;
-import static net.minecraft.util.EnumFacing.UP;
 import static tekcays_addon.api.consts.NBTKeys.HIT_NUMBER;
-import static tekcays_addon.api.consts.NBTKeys.IS_RUNNING;
 
 
 public class MetaTileEntityAxeSupport extends MetaTileEntity {
 
+    private IBlockState upperBlock;
     private int hitNumber = 0;
     private final int HIT_NUMBER_TO_TRANSFORM = 5;
+    private BlockPos upperBlockPos;
+    private BlockCutWood.CutWoodType woodType;
+    private IBlockState upperBlockState;
 
     public MetaTileEntityAxeSupport(ResourceLocation metaTileEntityId) {
         super(metaTileEntityId);
@@ -69,6 +51,7 @@ public class MetaTileEntityAxeSupport extends MetaTileEntity {
     @Override
     public void update() {
         super.update();
+
     }
 
     @Override
@@ -83,71 +66,69 @@ public class MetaTileEntityAxeSupport extends MetaTileEntity {
 
             Set<String> toolClasses = itemStack.getItem().getToolClasses(itemStack);
             if (toolClasses.isEmpty()) return true;
-
-            TKCYALog.logger.info(toolClasses);
-            if (!toolClasses.iterator().next().equals(ToolClasses.AXE)) return true;
-
-            TKCYALog.logger.info("hitNumber = {}", hitNumber);
-
             ToolHelper.damageItem(itemStack, playerIn);
-            return onAxeClick();
+
+            upperBlockPos = getPos().offset(EnumFacing.UP);
+
+            if (isUpFaceFree()) return true;
+
+            upperBlockState = upperBlock.getBlock().getDefaultState();
+
+            if (toolClasses.iterator().next().equals(ToolClasses.AXE)) {
+                onAxeClick();
+            } else if (toolClasses.iterator().next().equals(ToolClasses.SAW)) {
+                onSawClick();
+            }
         }
-        return false;
+        return true;
     }
 
-    private void incrementAndTryDestroy(World world, BlockPos blockPos, BlockCutWood.CutWoodType cutWoodType) {
+    private boolean isUpFaceFree() {
+        World world = this.getWorld();
+        upperBlock = world.getBlockState(upperBlockPos);
+        return upperBlock.equals(Blocks.AIR.getDefaultState());
+    }
+
+    private void incrementAndTryDestroy(BlockPos blockPos, IBlockState toAdd) {
         hitNumber++;
         if (hitNumber == HIT_NUMBER_TO_TRANSFORM) {
-            world.setBlockState(blockPos, TKCYAMetaBlocks.BLOCK_CUT_WOOD.getState(cutWoodType));
+            getWorld().setBlockState(blockPos, toAdd);
             hitNumber = 0;
         }
     }
 
-    public boolean onAxeClick() {
+    private void onSawClick() {
 
-        World world = this.getWorld();
-        BlockPos blockPos = getPos().offset(EnumFacing.UP);
+        IBlockState plank = BlockCutWood.getPlankFromCutWood(upperBlock);
+        if (plank != null && BlockCutWood.getInfo(upperBlockState)[0].equals(BlockCutWood.TRANSLATION_KEY)) {
+            incrementAndTryDestroy(upperBlockPos, plank);
+            return;
+        }
 
-        IBlockState block = world.getBlockState(blockPos);
-        if (block.equals(Blocks.AIR.getDefaultState())) return true;
-
-        if (block.equals(Blocks.LOG2.getDefaultState())) {
-            incrementAndTryDestroy(world, blockPos, BlockCutWood.CutWoodType.ACACIA);
-            return true;
-        } else if (block.equals(Blocks.LOG.getDefaultState().withProperty(BlockOldLog.VARIANT, BlockPlanks.EnumType.OAK))) {
-            incrementAndTryDestroy(world, blockPos, BlockCutWood.CutWoodType.OAK);
-            return true;
-        } else if (block.equals(Blocks.LOG.getDefaultState().withProperty(BlockOldLog.VARIANT, BlockPlanks.EnumType.JUNGLE))) {
-            incrementAndTryDestroy(world, blockPos, BlockCutWood.CutWoodType.JUNGLE);
-            return true;
-        } else if (block.equals(Blocks.LOG.getDefaultState().withProperty(BlockOldLog.VARIANT, BlockPlanks.EnumType.BIRCH))) {
-            incrementAndTryDestroy(world, blockPos, BlockCutWood.CutWoodType.BIRCH);
-            return true;
-        } else if (block.equals(Blocks.LOG.getDefaultState().withProperty(BlockOldLog.VARIANT, BlockPlanks.EnumType.SPRUCE))) {
-            incrementAndTryDestroy(world, blockPos, BlockCutWood.CutWoodType.SPRUCE);
-            return true;
-        } else if (block.equals(MetaBlocks.RUBBER_LOG.getDefaultState())) {
-            incrementAndTryDestroy(world, blockPos, BlockCutWood.CutWoodType.RUBBER);
-            return true;
+        IBlockState slab = BlockCutWood.getSlabFromPlank(upperBlock);
+        if (plank != null && BlockCutWood.getInfo(upperBlockState)[0].equals(BlockCutWood.PLANK)) {
+            incrementAndTryDestroy(upperBlockPos, slab);
+            return;
         }
 
         hitNumber = 0;
-        return true;
+    }
+
+    private void onAxeClick() {
+        woodType = BlockCutWood.getCutWoodTypeFromLog(upperBlock);
+
+        if (woodType != null) {
+            IBlockState toAdd = TKCYAMetaBlocks.BLOCK_CUT_WOOD.getState(woodType);
+            incrementAndTryDestroy(upperBlockPos, toAdd);
+            return;
+        }
+        hitNumber = 0;
     }
 
     @SideOnly(Side.CLIENT)
     protected SimpleOverlayRenderer getBaseRenderer() {
         return TKCYATextures.STEAM_CASING[1];
     }
-
-    /*
-    @Override
-    @SideOnly(Side.CLIENT)
-    public SoundEvent getSound() {
-        return ToolItems.AXE.getSound();
-    }
-
-     */
 
     @Override
     public void renderMetaTileEntity(CCRenderState renderState, Matrix4 translation, IVertexOperation[] pipeline) {
