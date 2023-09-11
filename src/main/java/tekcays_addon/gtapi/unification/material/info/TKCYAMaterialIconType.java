fff6907422ffa27daab3147b757d424dac4c2b1f
Pierre <pierreblc@hotmail.fr>
2023-09-11 18:01:26 +0200

    added ore entry and texture for the rotor blade

--- src/main/java/tekcays_addon/gtapi/unification/material/info/TKCYAMaterialIconType.java
+++ src/main/java/tekcays_addon/gtapi/unification/material/info/TKCYAMaterialIconType.java
@@ -24,5 +24,6 @@ public class TKCYAMaterialIconType {
 
     // Others
     public static final MaterialIconType curvedPlate = new MaterialIconType("curvedPlate");
+    public static final MaterialIconType blade = new MaterialIconType("blade");
 
 }
